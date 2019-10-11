0a3656faec8c45adc72b57bdebf8fb13a1eb6f59
liaosir123 <49466534+liaosir123@users.noreply.github.com>
2019-10-11 21:42:46 +0800

    Update HelloWorldController.java

--- src/main/java/io/kubesphere/devops/HelloWorldController.java
+++ src/main/java/io/kubesphere/devops/HelloWorldController.java
@@ -14,6 +14,6 @@ public class HelloWorldController {
 
     @RequestMapping("/")
     public String sayHello() {
-        return "Really appreaciate your star , that's the power of our life,now take this is test";
+        return "测试一下ss";
     }
 }
